d908a7a46b5dae1122a7f23314de32a989448292
Markus Schmall <schmall.markus@gmail.com>
2015-08-25 21:29:23 +0200

    added PUT method handler

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -8,5 +8,12 @@ class UrlMappings {
         }
 
         "/"(controller: "Main", action:"index")
+
+
+        "/$index/$object/$id" (controller: "Main") {
+            action = [GET: "get", PUT: "put", DELETE: "delete", POST: "post"]
+        }
+
+
 	}
 }
--- grails-app/controllers/elasticpot/MainController.groovy
+++ grails-app/controllers/elasticpot/MainController.groovy
@@ -3,10 +3,19 @@ package elasticpot
 class MainController
 {
 
+    /**
+     *
+     *
+     *
+     *
+     *
+     */
+
+
     def index()
     {
 
-        print "Request from IP " + request.getRemoteAddr() + " at time " + new Date()
+        println "ElasticPot: Index: Request from IP " + request.getRemoteAddr() + " at time " + new Date()
 
         response.setContentType("application/json")
         def data = " { \"status\" : 200,"+
@@ -23,4 +32,38 @@ class MainController
                         "}"
         render(data)
     }
+
+    def put()
+    {
+        response.setContentType("text/plain")
+
+        def requestLine = request.forwardURI
+        println "PUT: " + requestLine
+        render(requestLine)
+
+        println "ElasticPot: Put Call: Index: " + params.index + " Object: " + params.object + " ID: " + params.id + " at time " + new Date()
+    }
+
+
+    def get()
+    {
+
+    }
+
+    def post()
+    {
+
+    }
+
+    def delete()
+    {
+
+    }
 }
+
+/*
+
+BE1NR176:ElasticSearch a790100$ curl -XPUT 'http://localhost:9200/ews/Alert/5468215b0cf210243d8c3841' -d '{ "user" : "kimchy", "post_date" : "2007-11-15T14:12:12", "message" : "trying out Elasticsearch" }'
+{"_index":"ews","_type":"Alert","_id":"5468215b0cf210243d8c3841","_version":4,"created":false}BE1NR176:ElasticSearch a790100$
+
+ */
